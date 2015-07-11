271e81cf7cb7eb0a8174dcf795665ee9bad9ffcd
selasiehanson <soundfever18@gmail.com>
2015-07-11 17:14:55 +0000

    Added other pages and images

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,5 +1,5 @@
 module Settings
-  REGISTRATION_END_DATE = "2014-10-17"
+  REGISTRATION_END_DATE = "2015-08-12"
 
   def Settings.registration_ended?
     Date.today > Date.parse(REGISTRATION_END_DATE)
