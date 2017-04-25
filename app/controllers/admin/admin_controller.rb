a2371b1cb86c9b59d8b66c9b5b650ecff133e514
selasie hanson <soundfever18@gmail.com>
2017-04-25 18:52:57 +0000

    Changes gem versions. Ugrades to ruby 2.2. Adds security to admin page

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,14 @@
-class Admin::AdminController < ApplicationController  
+class Admin::AdminController < ApplicationController
   layout false
+
   before_filter -> {
     # TODO:
-    puts "check if user is admin"
+    if (signed_in? && !current_user.is_admin) || !signed_in?
+      flash.now[:danger] = "Sorry admin page is for admins only"
+      redirect_to root_url
+    end
   }
+
 
   def index
     render layout: 'admin', nothing: true
