c88de3a8c7211b4ef703e912ad35041574c9eeff
lazaraus <joe@lazaraus>
2020-03-16 12:06:54 -0400

    checkpoint 2020

--- app/assets/vue/app.js
+++ app/assets/vue/app.js
@@ -5,29 +5,29 @@ axios.defaults.headers.common['Accept'] = 'application/json'
 
 var app = new Vue({
     el: '#app',
-    data:function() {
+    data: function () {
         return {
             campers: []
         }
 
     },
     mounted: function () {
-     var fetchedData= this.fetchData();
-     this.campers.push.apply(this.campers,fetchedData);
-     console.log("mounting happens here");
-     console.log(fetchedData);
+        var fetchedData = this.fetchData();
+        this.campers.push.apply(this.campers, fetchedData);
+        console.log("mounting happens here");
+        console.log(fetchedData);
 
     },
 
     methods: {
-        fetchData: function() {
-	 var res=this;
-         axios.get("api/campers").then(function(response){
-	  	res.campers=response.data;
-		console.log(res);
-	});
-	
-	
+        fetchData: function () {
+            var res = this;
+            axios.get("api/campers").then(function (response) {
+                res.campers = response.data;
+                console.log(res);
+            });
+
+
         }
 
     }
